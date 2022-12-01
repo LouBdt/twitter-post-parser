35a13190c2e204172d20da6d1ecc1ead10403288
LouBdt <81627775+LouBdt@users.noreply.github.com>
2022-12-01 15:35:32 +0100

    Added the stats of frenquency of words use

--- postparserforthreads.py
+++ postparserforthreads.py
@@ -53,14 +53,15 @@ def extraireDate(string):
             start_date = True
     return annee+" "+date
 
-stats = [[">Nombre de tweets totaux (dont réponses et RTs)",0], 
-         [">Nombre de retweets",0],
-         [">Nombre de threads >2tweets", 0],
-         [">Nombre de reponses a autrui", 0]]
+stats = [[">Nombre de tweets totaux (dont réponses et RTs)",0," (total number of tweets (incl. responses and RTs))"], 
+         [">Nombre de retweets",0, " (total number of retweets)"],
+         [">Nombre de threads >2tweets", 0, " (number of threads containing more than 2 tweets)"],
+         [">Nombre de reponses a autrui", 0, " (number of responses to someone else)"]]
 autrestweets = []
 retweets = []
 reponses = []
 threads = []
+
 #Avant de lire les fichiers on va les mettre dans une liste par ordre chronologique
 print("Scanning .md files (not the DM ones don\'t worry)...")
 fichiers = []
@@ -73,6 +74,8 @@ fichiers = sortByColumn(fichiers, 1)
 
 print("\r \n")
 print("\r Extracting tweets from markdown files...\n", end='')
+allwords = []
+mots_longs = []
 for file in fichiers:
     file = file[0]
     line = file.readline()
@@ -82,13 +85,24 @@ for file in fichiers:
             if "RT @" in tweet[0]:
                 retweets.append(tweet)
                 stats[1][1] +=1
-            elif "Replying to ["+user in tweet[0]:
-                threads.append(tweet)
-            elif "Replying to [@" in tweet[0]:
-                reponses.append(tweet)
-                stats[3][1] += 1
             else:
-                autrestweets.append(tweet)
+                phrase = [i for i in tweet if i[0]==">"]
+                for l in phrase:
+                    for mot in l.split(" "):
+                        if mot !=">" and mot != "" and mot[0]!="#" :
+                            if mot[0]!="@" and mot[:4]!='http' and mot[0]!="?":
+                                if mot[0]!="-" and mot[0]!="." and mot[0]!="!":
+                                    if mot[0]!="&" :
+                                        allwords.append(mot.lower())
+                                        if len(mot)>5:
+                                            mots_longs.append(mot.lower())
+                if "Replying to ["+user in tweet[0]:
+                    threads.append(tweet)
+                if "Replying to [@" in tweet[0]:
+                    reponses.append(tweet)
+                    stats[3][1] += 1
+                else:
+                    autrestweets.append(tweet)
             stats[0][1]+=1
             print("\r Tweet #"+str(stats[0][1]), end='')
             tweet = []
@@ -99,6 +113,21 @@ for file in fichiers:
         line = file.readline()
         
     file.close()
+
+print("\n Making stats of most-used words")
+listeuniquemots = list(set(allwords))
+stats_mots = []
+for mot in listeuniquemots:
+    stats_mots.append([mot, allwords.count(mot)])
+top_mot = sortByColumn(stats_mots, 1)
+top_mot.reverse()
+
+stats_mots_longs = []
+for mot in listeuniquemots:
+    stats_mots_longs.append([mot, mots_longs.count(mot)])
+top_mots_longs = sortByColumn(stats_mots_longs, 1)
+top_mots_longs.reverse()
+    
 
 print("\r \n")
 print("\r Re-organizing threads...\n", end='')
@@ -228,19 +257,39 @@ print("\n")
 import shutil
 #Sauvegarde des résultats
 cwd = os.getcwd()
-target_dir1 = cwd +'/Resultats'
+target_dir1 = cwd +'/Results'
 if not os.path.exists(target_dir1):
     os.mkdir(target_dir1)
-if input("Effaçage des données de "+target_dir1+" ? [y/n]").lower()=="y":
-    print("Sauvegarde des résultats")
+if input("Clear datas from "+target_dir1+" ? [y/n]").lower()=="y":
+    print("Saving results...")
     shutil.rmtree(target_dir1)
     if not os.path.exists(target_dir1):
         os.mkdir(target_dir1)
     with open(target_dir1+'/'+'stats.txt', 'w') as f:
         f.write('===Statistiques de l\'archive twitter de '+user+'===\n')
+        f.write('===Statistics for user '+user+'\'s archive===\n')
         for ligne in stats:
-            f.write(ligne[0]+': '+str(ligne[1]))
+            f.write(ligne[0]+': '+str(ligne[1])+ligne[2])
             f.write('\n')
+        f.write('\n')
+        f.write('\n')
+        f.write('Nombre de mots utilisés :'+str(len(allwords))+" (number of words used) \n")
+        f.write("Top 100 des mots les plus utilisés : (Top 100 most used words) \n" )
+        classement = 1
+        for mot in top_mot[:100]:
+            f.write(str(classement)+"- "+mot[0]+": "+str(mot[1])+'\n')
+            classement +=1
+        
+        
+        f.write('\n')
+        f.write('\n')
+        f.write('Nombre de mots de plus de 5 lettres utilisés :'+str(len(mots_longs))+" (number of words used (more than 5 letters)) \n")
+        f.write("Top 100 des mots de plus de 5 lettres les plus utilisés : (Top 100 most used words >5 letters) \n" )
+        classement = 1
+        for mot in top_mots_longs[:100]:
+            f.write(str(classement)+"- "+mot[0]+": "+str(mot[1])+'\n')
+            classement +=1
+            
     target_dir2 = target_dir1+'/Threads'
     if not os.path.exists(target_dir2):
         os.mkdir(target_dir2)
@@ -252,7 +301,7 @@ if input("Effaçage des données de "+target_dir1+" ? [y/n]").lower()=="y":
                 else:
                     f.write(t[1]+'\n')
                 f.write('\n')
-    print("Résultats sauvegardés dans "+target_dir1)
+    print("Results saved in "+target_dir1)
 else:
-    print("Sauvegarde des résultats impossible !")
-    print("Fin du programme")
+    print("Impossible to save the result")
+    print("End")
